db5509b56fe44f6361873e7560818223e67c9535
Gamaliel Adun <adungamaliel@gmail.com>
2024-10-04 18:22:17 +0100

    implement DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import "./style.css";
 
+const city = document.querySelector("#city");
+const time = document.querySelector("#time");
+const currentTemperature = document.querySelector("#current-temp");
+const weatherCondition = document.querySelector("#weather-condition");
+const windSpeed = document.querySelector("#wind-speed");
+const rainProbability = document.querySelector("#rain-probability");
+const sunlightHours = document.querySelector("#sunlight-hours");
+const weatherIcon = document.querySelector("#weather-icon");
+
 async function getWeather(location) {
   try {
     const weather = await fetch(
@@ -11,15 +20,42 @@ async function getWeather(location) {
       );
     }
     const weatherResponse = await weather.json();
-    console.log(extractWeatherInfo(weatherResponse));
+    console.log(weatherResponse.currentConditions);
+    updateWeatherInfo(weatherResponse);
   } catch (error) {
     console.error(`Failed to fetch weather data: ${error.message}`);
     throw Error(`Location ${location} doesn't exist`);
   }
 }
 
-function extractWeatherInfo(response) {
+function updateWeatherInfo(response) {
+  city.textContent = response.address;
+  time.textContent = response.currentConditions.datetime;
+  currentTemperature.textContent = `${response.currentConditions.temp}°F`;
+  weatherCondition.textContent = response.currentConditions.conditions;
+  windSpeed.textContent = `${response.currentConditions.windspeed} km/h`;
+  rainProbability.textContent = `${response.currentConditions.precipprob} %`;
+  sunlightHours.textContent = `${getTimeDifferenceInHours(
+    response.currentConditions.sunset,
+    response.currentConditions.sunrise
+  )} hrs`;
   return response.currentConditions;
+}
+
+function convertToTimeObject(timeString) {
+  const [hours, minutes, seconds] = timeString.split(":").map(Number);
+  const date = new Date();
+  date.setHours(hours, minutes, seconds, 0);
+  return date;
+}
+
+function getTimeDifferenceInHours(timeString1, timeString2) {
+  const time1 = convertToTimeObject(timeString1);
+  const time2 = convertToTimeObject(timeString2);
+
+  const diffMilliseconds = Math.abs(time2 - time1);
+  const diffHours = diffMilliseconds / (1000 * 60 * 60);
+  return Math.round(diffHours);
 }
 
 getWeather("Abuja");
